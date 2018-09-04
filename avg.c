e8c13e9adf75ea265bae74746f2f46a58e5c6526
ov37 <ov37@cornell.edu>
2018-09-03 22:35:11 -0400

    actual files for sec2

---
+++ avg.c
@@ -0,0 +1,7 @@
+#include "avg.h"
+
+int avg( int x, int y )
+{
+  int sum = x + y;
+  return sum/2;
+}
---
+++ avg.h
@@ -0,0 +1,6 @@
+#ifndef TUT3_C_AVG_H
+#define TUT3_C_AVG_H
+
+int avg(int x, int y);
+
+#endif
